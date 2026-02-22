c3b517b0e778945beb7c5c03a45196a1c7398621
skmai <150113759+skmai@users.noreply.github.com>
2026-02-22 08:59:55 +0530

    Create theme.dart

---
+++ lib/core/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    primarySwatch: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.blue,
+      centerTitle: true,
+    ),
+  );
+}
